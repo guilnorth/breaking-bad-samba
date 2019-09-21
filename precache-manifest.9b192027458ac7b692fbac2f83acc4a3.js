4fb314653b05a5a7693d0add89faaf4237151421
Guilherme <ggn.noronha@gmail.com>
2019-09-21 13:26:46 -0300

    Updates

---
+++ precache-manifest.9b192027458ac7b692fbac2f83acc4a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c17b2d72b4d2f1994d21e01ecafe9bac",
+    "url": "/breaking-bad-samba/index.html"
+  },
+  {
+    "revision": "6c7b4c63d190600acae3",
+    "url": "/breaking-bad-samba/static/css/2.22d6b5ec.chunk.css"
+  },
+  {
+    "revision": "7bb864755c9573866eb3",
+    "url": "/breaking-bad-samba/static/css/main.7185b011.chunk.css"
+  },
+  {
+    "revision": "6c7b4c63d190600acae3",
+    "url": "/breaking-bad-samba/static/js/2.2e7ce9cf.chunk.js"
+  },
+  {
+    "revision": "7bb864755c9573866eb3",
+    "url": "/breaking-bad-samba/static/js/main.6a3af341.chunk.js"
+  },
+  {
+    "revision": "a87dd7fee887b671790c",
+    "url": "/breaking-bad-samba/static/js/runtime~main.dd73f2ea.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/breaking-bad-samba/static/media/Breaking_Bad_logo.427a4adc.svg"
+  }
+]);
